24598234224b389d5a78659237b9138400cd3e3d
swethaogeti <65771591+swethaogeti@users.noreply.github.com>
2022-09-22 15:05:30 +0530

    feat : logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
 import { SortBox } from "./SortBox";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { SidebarMobileView } from "./SidebarMobileView";
 export const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className=" sticky top-0 z-10 bg-white shadow-sm ">
       <div className="flex w-full items-center justify-between mx-auto p-3  ">
@@ -26,9 +27,13 @@ export const Navbar = () => {
             <SortBox className="absolute " />
           </div>
           <div className="flex space-x-2 items-center cursor-pointer flex-col">
-            <Avatar></Avatar>
-
-            <button className="text-purple-600 font-bold">LogOut</button>
+            <Avatar></Avatar>{" "}
+            <button
+              className="text-purple-600 font-bold"
+              onClick={() => navigate("/")}
+            >
+              LogOut
+            </button>
           </div>
         </div>
       </div>
